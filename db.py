4e3d0b03845de4577dd18eaafd43d1e234010cc7
mouatezdira-coder <mouatezdira@gmail.com>
2026-05-12 20:24:44 +0200

    Update db.py

--- db.py
+++ db.py
@@ -2,9 +2,6 @@ import psycopg2
 
 def connect():
     conn = psycopg2.connect(
-        dbname="The Wave",             
-        user="postgres",
-        password="mouatezGustave#18", 
-        host="localhost"
+
     )
     return conn
