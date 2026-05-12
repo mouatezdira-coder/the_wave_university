ccd03316c50388f4c898a32a67f717ab6a97cb9e
mouatezdira-coder <mouatezdira@gmail.com>
2026-05-12 20:50:40 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,17 @@
 import psycopg2
-
+import os
 def connect():
-    conn = psycopg2.connect(
-
-    )
-    return conn
+    try:
+        conn = psycopg2.connect(
+            # On récupère les infos depuis les variables d'environnement
+            host=os.environ.get("DB_HOST"),
+            database="postgres",
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            port="6543",
+            sslmode="require"  # Très important pour le cloud !
+        )
+        return conn
+    except Exception as e:
+        print(f"Error connecting to Supabase: {e}")
+        return None
