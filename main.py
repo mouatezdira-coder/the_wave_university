d27e314be4c9c5e5131af91ccd66b3a2126d1596
mouatezdira-coder <mouatezdira@gmail.com>
2026-05-12 21:00:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import db
 from datetime import timedelta, datetime
 from werkzeug.security import check_password_hash, generate_password_hash
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = 'projet_thewave_secret' 
